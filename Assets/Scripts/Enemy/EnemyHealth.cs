46d79e0a47adf88926bdb915fd29879236f1d498
taggeddev <denis.mikov.06@gmail.com>
2021-10-28 13:02:10 +0300

    feat: Added hit, dying, dead animations for viking and the conditions to play them

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,10 +6,12 @@ public class EnemyHealth : Health
 {
     protected override void Death()
     {
-        GetComponent<SpriteRenderer>().color = Color.grey;
+        //GetComponent<SpriteRenderer>().color = Color.grey;
         GetComponent<EnemyAttack>().enabled = false;
         GetComponent<EnemyMovement>().enabled = false;
         GetComponent<EnemyHealth>().enabled = false;
+        GetComponent<Animator>().SetTrigger("dying");
+
 
         gameObject.AddComponent(typeof(EnemyCanBeDrained));
         GetComponent<EnemyCanBeDrained>().healthToHeal = GetComponent<EnemyHealth>().GetMaxHp() * 0.1f;
