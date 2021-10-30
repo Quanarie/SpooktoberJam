079f2918eceadc261b6dbf8e2861546b01fdc8f4
sppeeedd <qustaine@gmail.com>
2021-10-30 19:37:17 +0300

    Enemies death added

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -5,15 +5,44 @@ using UnityEngine;
 public class EnemyHealth : Health
 {
     private const float coefficientToRegen = 0.1f;
+    private const float timeTakingDamage = 1f;
+    private SpriteRenderer spriteRenderer;
+    private Color color;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        color = spriteRenderer.color;
+    }
+
+    public override void ReceiveDamage(float damage, Vector3 pushDirection, float pushForce, HealthBar healthBar = null)
+    {
+        base.ReceiveDamage(damage, pushDirection, pushForce, healthBar);
+
+        spriteRenderer.color = Color.white;
+
+        StartCoroutine(ChangeColorBack());
+    }
+
+    IEnumerator ChangeColorBack()
+    {
+        yield return new WaitForSeconds(timeTakingDamage);
+        spriteRenderer.color = color;
+    }
 
     protected override void Death()
     {
-        //GetComponent<SpriteRenderer>().color = Color.grey;
+        if (GetComponent<Animator>().GetBool("isDead"))
+            return;
+
         GetComponent<EnemyAttack>().enabled = false;
         GetComponent<EnemyMovement>().enabled = false;
         GetComponent<EnemyHealth>().enabled = false;
         GetComponent<Animator>().SetBool("isDead", true);
-        GetComponent<Animator>().SetTrigger("death");
+        transform.localScale = new Vector3(transform.localScale.x/2, transform.localScale.y, transform.localScale.z);
+        transform.eulerAngles = new Vector3(0, 0, 90);
 
 
         gameObject.AddComponent(typeof(EnemyCanBeDrained));
